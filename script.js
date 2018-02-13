a4955303d4976b38a3fad746205614def01635e3
Inna Vashchuk <vashchuk.inna@gmail.com>
2018-02-13 22:07:24 +0200

    rest parameters

--- script.js
+++ script.js
@@ -274,3 +274,43 @@ const h = document.querySelector('h1');
 const all = [h, ...boxes];
 
 Array.from(all).forEach(cur => cur.style.color = 'purple');
+
+//Rest parameters
+
+//ES5
+function isFullAge5() {
+    //console.log(arguments);
+    var argsArr = Array.prototype.slice.call(arguments);
+
+    argsArr.forEach(function (cur) {
+        console.log((2016 - cur) >= 18)
+    })
+}
+
+isFullAge5(1990, 1999, 1965);
+
+//ES6
+function isFullAge6(...years) {
+    years.forEach(cur => console.log((2018 - cur) >= 18));
+}
+
+isFullAge6(1990, 1999, 1965);
+
+
+//ES5
+function isFullAge5(limit) {
+    var argsArr = Array.prototype.slice.call(arguments, 1);
+
+    argsArr.forEach(function (cur) {
+        console.log((2016 - cur) >= limit)
+    })
+}
+
+isFullAge5(16, 1990, 1999, 1965);
+
+//ES6
+function isFullAge6(limit, ...years) {
+    years.forEach(cur => console.log((2018 - cur) >= limit));
+}
+
+isFullAge6(1990, 1999, 1965);
