2917d5bb00ca4e8fcfd652389db49c3c6ce36bce
Inna Vashchuk <vashchuk.inna@gmail.com>
2018-02-23 12:32:43 +0200

    classes with subclasses

--- script.js
+++ script.js
@@ -388,6 +388,56 @@ Person5.prototype.calculateAge = function () {
 var john5 = new Person('John', 1990, 'teacher');
 
 //ES6
+/*class Person6 {
+ constructor(name, yearOfBirth, job) {
+ this.name = name;
+ this.yearOfBirth = yearOfBirth;
+ this.job = job;
+ }
+
+ calculateAge() {
+ var age = new Date().getFullYear() - this.yearOfBirth;
+ console.log(age);
+ }
+
+ static greeting() {
+ console.log('Hey there!');
+ }
+ }
+
+ const john6 = new Person('John', 1990, 'teacher');
+ Person6.greeting();*/
+
+//Classes and subclasses
+//ES5
+var Person5 = function (name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+};
+
+Person5.prototype.calculateAge = function () {
+    var age = new Date().getFullYear() - this.yearOfBirth;
+    console.log(age);
+};
+
+var Athlete5 = function (name, yearOfBirth, job, olympicGames, medals) {
+    Person5.call(this, name, yearOfBirth, job);
+    this.olympicGames = olympicGames;
+    this.medals = medals;
+};
+
+Athlete5.prototype = Object.create(Person5.prototype);
+
+Athlete5.prototype.wonMedal = function () {
+    this.medals++;
+    console.log(this.medals);
+};
+
+var johnAthlete5 = new Athlete5('John', 1990, 'swimmer', 3, 10);
+johnAthlete5.calculateAge();
+
+//ES6
 class Person6 {
     constructor(name, yearOfBirth, job) {
         this.name = name;
@@ -399,11 +449,22 @@ class Person6 {
         var age = new Date().getFullYear() - this.yearOfBirth;
         console.log(age);
     }
+}
 
-    static greeting() {
-        console.log('Hey there!');
+class Athlete6 extends Person6 {
+    constructor(name, yearOfBirth, job, olympicGames, medals) {
+        super(name, yearOfBirth, job);
+        this.olympicGames = olympicGames;
+        this.medals = medals;
+    }
+
+    wonMedal() {
+        this.medals++;
+        console.log(this.medals);
     }
 }
 
-const john6 = new Person('John', 1990, 'teacher');
-Person6.greeting();
+const johnAthlete6 = new Athlete6('John', 1990, 'swimmer', 3, 10);
+
+johnAthlete6.wonMedal();
+johnAthlete6.calculateAge();
