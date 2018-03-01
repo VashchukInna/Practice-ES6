b4e2ec6089fb6c3f62162939db36bc656922ca8b
Inna Vashchuk <vashchuk.inna@gmail.com>
2018-03-01 20:44:56 +0200

    init

--- script-transpiled.js
+++ script-transpiled.js
@@ -1,3 +1,15 @@
+'use strict';
+
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
 //ES5
 /*var name5 = 'Jane Smith';
  var age5 = 23;
@@ -488,92 +500,128 @@
 
  */
 
-class Element {
-    constructor(name, buildYear) {
-        this.name = name;
-        this.buildYear = buildYear;
+var Element = function Element(name, buildYear) {
+  _classCallCheck(this, Element);
+
+  this.name = name;
+  this.buildYear = buildYear;
+};
+
+var Park = function (_Element) {
+  _inherits(Park, _Element);
+
+  function Park(name, buildYear, area, numTrees) {
+    _classCallCheck(this, Park);
+
+    var _this = _possibleConstructorReturn(this, (Park.__proto__ || Object.getPrototypeOf(Park)).call(this, name, buildYear));
+
+    _this.area = area;
+    _this.numTrees = numTrees;
+    return _this;
+  }
+
+  _createClass(Park, [{
+    key: 'treeDensity',
+    value: function treeDensity() {
+      var density = this.numTrees / this.area;
+      console.log(this.name + ' has a tree density of ' + density + ' trees per square km.');
     }
-}
+  }]);
 
+  return Park;
+}(Element);
 
-class Park extends Element {
-    constructor(name, buildYear, area, numTrees) {
-        super(name, buildYear);
-        this.area = area;
-        this.numTrees = numTrees;
+var Street = function (_Element2) {
+  _inherits(Street, _Element2);
+
+  function Street(name, buildYear, length) {
+    var size = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 3;
+
+    _classCallCheck(this, Street);
+
+    var _this2 = _possibleConstructorReturn(this, (Street.__proto__ || Object.getPrototypeOf(Street)).call(this, name, buildYear));
+
+    _this2.length = length;
+    _this2.size = size;
+    return _this2;
+  }
+
+  _createClass(Street, [{
+    key: 'classifyStreet',
+    value: function classifyStreet() {
+      var classification = new Map();
+      classification.set(1, 'tiny');
+      classification.set(2, 'small');
+      classification.set(3, 'normal');
+      classification.set(4, 'big');
+      classification.set(5, 'huge');
+      console.log(this.name + ', build in ' + this.buildYear + ', is a ' + classification.get(this.size) + ' street.');
     }
+  }]);
 
-    treeDensity() {
-        const density = this.numTrees / this.area;
-        console.log(`${this.name} has a tree density of ${density} trees per square km.`);
-    }
-}
+  return Street;
+}(Element);
 
+var allParks = [new Park('Green Park', 1987, 0.2, 215), new Park('National Park', 1894, 2.9, 3541), new Park('Oak Park', 1953, 0.4, 949)];
 
-class Street extends Element {
-    constructor(name, buildYear, length, size = 3) {
-        super(name, buildYear);
-        this.length = length;
-        this.size = size;
-    }
-
-    classifyStreet() {
-        const classification = new Map();
-        classification.set(1, 'tiny');
-        classification.set(2, 'small');
-        classification.set(3, 'normal');
-        classification.set(4, 'big');
-        classification.set(5, 'huge');
-        console.log(`${this.name}, build in ${this.buildYear}, is a ${classification.get(this.size)} street.`);
-    }
-}
-
-
-const allParks = [new Park('Green Park', 1987, 0.2, 215),
-    new Park('National Park', 1894, 2.9, 3541),
-    new Park('Oak Park', 1953, 0.4, 949)];
-
-const allStreets = [new Street('Ocean Avenue', 1999, 1.1, 4),
-    new Street('Evergreen Street', 2008, 2.7, 2),
-    new Street('4th Street', 2015, 0.8),
-    new Street('Sunset Boulevard', 1982, 2.5, 5)];
-
+var allStreets = [new Street('Ocean Avenue', 1999, 1.1, 4), new Street('Evergreen Street', 2008, 2.7, 2), new Street('4th Street', 2015, 0.8), new Street('Sunset Boulevard', 1982, 2.5, 5)];
 
 function calc(arr) {
-    const sum = arr.reduce((prev, cur, index) => prev + cur, 0);
-    return [sum, sum / arr.length];
+  var sum = arr.reduce(function (prev, cur, index) {
+    return prev + cur;
+  }, 0);
+  return [sum, sum / arr.length];
 }
-
 
 function reportParks(p) {
 
-    console.log('-----PARKS REPORT-----');
+  console.log('-----PARKS REPORT-----');
 
-    //Density
-    p.forEach(el => el.treeDensity());
+  //Density
+  p.forEach(function (el) {
+    return el.treeDensity();
+  });
 
-    //Average age
-    const ages = p.map(el => new Date().getFullYear() - el.buildYear);
-    const [totalAge, avgAge] = calc(ages);
-    console.log(`Our ${p.length} parks have an average of ${avgAge} years.`);
+  //Average age
+  var ages = p.map(function (el) {
+    return new Date().getFullYear() - el.buildYear;
+  });
 
-    //Which park has more than 1000 trees
-    const i = p.map(el => el.numTrees).findIndex(el => el >= 1000);
-    console.log(`${p[i].name} has more than 1000 trees.`);
+  var _calc = calc(ages),
+      _calc2 = _slicedToArray(_calc, 2),
+      totalAge = _calc2[0],
+      avgAge = _calc2[1];
 
+  console.log('Our ' + p.length + ' parks have an average of ' + avgAge + ' years.');
+
+  //Which park has more than 1000 trees
+  var i = p.map(function (el) {
+    return el.numTrees;
+  }).findIndex(function (el) {
+    return el >= 1000;
+  });
+  console.log(p[i].name + ' has more than 1000 trees.');
 }
-
 
 function reportStreets(s) {
 
-    console.log('-----STREETS REPORT-----');
+  console.log('-----STREETS REPORT-----');
 
-    //Total and average length of the town's streets
-    const [totalLength, avgLength] = calc(s.map(el => el.length));
-    console.log(`Our ${s.length} streets have a total length of ${totalLength} km, with an average of ${avgLength} km.`);
+  //Total and average length of the town's streets
 
-    //Classify sizes
-    s.forEach(el => el.classifyStreet());
+  var _calc3 = calc(s.map(function (el) {
+    return el.length;
+  })),
+      _calc4 = _slicedToArray(_calc3, 2),
+      totalLength = _calc4[0],
+      avgLength = _calc4[1];
+
+  console.log('Our ' + s.length + ' streets have a total length of ' + totalLength + ' km, with an average of ' + avgLength + ' km.');
+
+  //Classify sizes
+  s.forEach(function (el) {
+    return el.classifyStreet();
+  });
 }
 
 reportParks(allParks);
